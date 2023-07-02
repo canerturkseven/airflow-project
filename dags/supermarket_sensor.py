865515c43d0a26f0f84517b1f18cf48b0b8f2420
Caner Turkseven <canerturkseven@gmail.com>
2023-07-02 20:26:14 +0200

    sensor example

--- dags/supermarket_sensor.py
+++ dags/supermarket_sensor.py
@@ -7,7 +7,7 @@ from airflow import DAG
 
 
 def _wait_for_supermarket(supermarket_id):
-    supermarket_path = Path("/data/" + supermarket_id)
+    supermarket_path = Path("./data/supermarket_" + supermarket_id)
     data_files = supermarket_path.glob("data-*.csv")
     success_file = supermarket_path / "_SUCCESS"
     return data_files and success_file.exists()
@@ -17,17 +17,17 @@ dag = DAG(
     dag_id="supermarket_data_cleanse",
     start_date=dt.datetime(year=2019, month=1, day=1),
     end_date=dt.datetime(year=2019, month=1, day=15),
-    schedule_interval="@daily",
+    schedule_interval=dt.timedelta(minutes=10),
     catchup=True,
 )
 wait_for_supermarket_1 = FileSensor(
     task_id="wait_for_supermarket_1",
-    filepath="/supermarkets/supermarket1/data.csv",
+    filepath="/data/supermarket1/data.csv",
     dag=dag,
 )
 wait_for_supermarket_2 = FileSensor(
     task_id="wait_for_supermarket_2",
-    filepath="/supermarkets/supermarket2/data.csv",
+    filepath="/data/supermarket2/data.csv",
     dag=dag,
 )
 cleanse_supermarket_1 = BashOperator(
