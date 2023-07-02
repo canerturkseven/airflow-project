b297696527d2179e4d7588bcc414777117ceae21
Caner Turkseven <canerturkseven@gmail.com>
2023-07-02 20:42:03 +0200

    remove unncessary import

--- dags/supermarket_sensor.py
+++ dags/supermarket_sensor.py
@@ -1,7 +1,6 @@
 import datetime as dt
 from pathlib import Path
 from airflow.sensors.filesystem import FileSensor
-from airflow.sensors.python import PythonSensor
 from airflow.operators.bash import BashOperator
 from airflow import DAG
 
@@ -11,7 +10,6 @@ def _wait_for_supermarket(supermarket_id):
     data_files = supermarket_path.glob("data-*.csv")
     success_file = supermarket_path / "_SUCCESS"
     return data_files and success_file.exists()
-
 
 dag = DAG(
     dag_id="supermarket_data_cleanse",
